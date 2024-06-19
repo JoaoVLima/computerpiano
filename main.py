05e9791a9fe91460a57f29d1af34ad3e1eb43d20
JoaoVLima <celu.joaolima@gmail.com>
2024-06-19 17:55:00 -0300

    trying to find a solution to have diferent octaves and diferent octaves on the keyboard

--- main.py
+++ main.py
@@ -20,67 +20,60 @@ import json
 # 120-131 octave 9
 NOTES = librosa.midi_to_note(range(12, 132))
 
-OCTAVE = 4
-FIRST_NOTE = 12 * OCTAVE
-
 with open('config.json') as config:
     config = json.loads(config.read())
 
 
-def main():
-    while True:
-        input()
+def list_notes(note_range):
+    # notes = 'C0'
+    # notes = 'C0-B9'
+    # notes = ['C0','C1']
+    # return = ['C0','C1']
+    if isinstance(note_range, str):
+        if '-' not in note_range:
+            return np.array([note_range])
+        start, end = note_range.split('-', 2)
+        start_note = librosa.note_to_midi(start)
+        end_note = librosa.note_to_midi(end)
+        return librosa.midi_to_note(range(start_note, end_note + 1))
+    return note_range
 
 
 class ComputerPiano:
-    def __init__(self, notes='C0-B9'):
-        self.notes = {note: None for note in self.list_of_notes(notes)}
+    def __init__(self, note_range='C0-B9'):
+        self.note_range = note_range
+        self.notes = {note: None for note in list_notes(note_range)}
 
-    def list_of_notes(self, note_range):
-        # notes = 'C0'
-        # notes = 'C0-B9'
-        # notes = ['C0','C1']
-        # return = ['C0','C1']
-        if isinstance(note_range, str):
-            if '-' not in note_range:
-                return np.array([note_range])
-            start, end = note_range.split('-', 2)
-            start_note = librosa.note_to_midi(start)
-            end_note = librosa.note_to_midi(end)
-            return librosa.midi_to_note(range(start_note, end_note+1))
-        return note_range
-
-    def add_instrument(self, instrument_instance):
-        list_of_notes = self.list_of_notes(instrument_instance.note_range)
+    def add_instrument(self, instrument_instance, note_range=None):
+        # instrument_instance.note_range = 'B0-A4'
+        # note_range = 'C0-A#3'
+        if not note_range:
+            note_range = instrument_instance.note_range
+            
+        list_of_notes = list_notes(note_range)
         for note in list_of_notes:
             self.notes[note] = instrument_instance
-
-
-    def play(self, note):
-        instrument = self.notes[note]
-        instrument.play(note)
 
 
 class Instrument:
     def __init__(self, name, note_range, octave_changer=True):
         self.name = name
         self.note_range = note_range
+        self.notes = {note: None for note in list_notes(note_range)}
         self.octave_changer = octave_changer
-
-    def play(self, notes):
-        print(self.name)
-        if isinstance(notes, str):
-            notes = [notes]
-        print(notes)
 
 
 class Piano(Instrument):
     def __init__(self):
-        super().__init__(name='Piano', note_range='C1-B2', octave_changer=True)
+        super().__init__(name='Piano', note_range='C0-B9', octave_changer=True)
+
+
+class Drums(Instrument):
+    def __init__(self):
+        super().__init__(name='Drums', note_range='B0-A4', octave_changer=False)
 
 
 if __name__ == '__main__':
     computer_piano = ComputerPiano()
-    piano = Piano()
-    computer_piano.add_instrument(piano)
-    computer_piano.play('C1')
+    computer_piano.add_instrument(Piano())
+    computer_piano.add_instrument(Drums(), 'C0')
