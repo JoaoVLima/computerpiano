0f3363dc0f55a9f3be82e7def0340194404a5077
JoaoVLima <celu.joaolima@gmail.com>
2024-06-25 15:48:46 -0300

    main

--- main.py
+++ main.py
@@ -1,114 +1,135 @@
 import math
 import itertools
-from collections import OrderedDict
 import numpy as np
 import pyaudio
-import pygame
 import librosa
-import json
+import threading
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
 
-with open('config.json') as config:
-    config = json.loads(config.read())
+BUFFER_SIZE = 64
+SAMPLE_RATE = 44100
+NOTE_AMP = 0.1
 
-# midinote = index+12
-# 12-23 octave 0
-# 24-35 octave 1
-# 36-47 octave 2
-# 48-47 octave 3
-# 60-71 octave 4
-# 72-83 octave 5
-# 84-95 octave 6
-# 96-107 octave 7
-# 108-119 octave 8
-# 120-131 octave 9
-
-NOTES = librosa.midi_to_note(range(12, 131+1), unicode=False)
-# ['C0',  'C♯0',  'D0',   'D♯0',  'E0',   'F0',   'F♯0',  'G0',   'G♯0',  'A0',
-#  'A♯0', 'B0',   'C1',   'C♯1',  'D1',   'D♯1',  'E1',   'F1',   'F♯1',  'G1',
-#  'G♯1', 'A1',   'A♯1',  'B1',   'C2',   'C♯2',  'D2',   'D♯2',  'E2',   'F2',
-#  'F♯2', 'G2',   'G♯2',  'A2',   'A♯2',  'B2',   'C3',   'C♯3',  'D3',   'D♯3',
-#  'E3',  'F3',   'F♯3',  'G3',   'G♯3',  'A3',   'A♯3',  'B3',   'C4',   'C♯4',
-#  'D4',  'D♯4',  'E4',   'F4',   'F♯4',  'G4',   'G♯4',  'A4',   'A♯4',  'B4',
-#  'C5',  'C♯5',  'D5',   'D♯5',  'E5',   'F5',   'F♯5',  'G5',   'G♯5',  'A5',
-#  'A♯5', 'B5',   'C6',   'C♯6',  'D6',   'D♯6',  'E6',   'F6',   'F♯6',  'G6',
-#  'G♯6', 'A6',   'A♯6',  'B6',   'C7',   'C♯7',  'D7',   'D♯7',  'E7',   'F7',
-#  'F♯7', 'G7',   'G♯7',  'A7',   'A♯7',  'B7',   'C8',   'C♯8',  'D8',   'D♯8',
-#  'E8',  'F8',   'F♯8',  'G8',   'G♯8',  'A8',   'A♯8',  'B8',   'C9',   'C♯9',
-#  'D9',  'D♯9',  'E9',   'F9',   'F♯9',  'G9',   'G♯9',  'A9',   'A♯9',  'B9']
-
-
-def list_notes(note_range):
-    # notes = 'C0'
-    # notes = 'C0-B9'
-    # notes = ['C0','C1']
-    # return = ['C0','C1']
-    if isinstance(note_range, str):
-        if '-' not in note_range:
-            return np.array([note_range])
-        start, end = note_range.split('-', 2)
-        start_note = librosa.note_to_midi(start)
-        end_note = librosa.note_to_midi(end)
-        return librosa.midi_to_note(range(start_note, end_note + 1), unicode=False)
-    return note_range
-
-
-class ComputerPiano:
-    def __init__(self, note_range='C0-B9'):
-        self.note_range = note_range
-        self.notes = {note: {'instrument': None, 'note': note} for note in list_notes(note_range)}
-
-    def add_instrument(self, instrument_instance, note_range=None):
-        # instrument_instance.note_range = 'B0-A4'
-        # note_range = 'C0-A#3'
-        if not note_range:
-            note_range = instrument_instance.note_range
-
-        list_of_notes_location = list_notes(note_range)
-        for note_location, note_instrument in zip(list_of_notes_location, instrument_instance.notes.keys()):
-            self.notes[note_location]['instrument'] = instrument_instance
-            self.notes[note_location]['note'] = note_instrument
-
-    def play(self, notes):
-        if isinstance(notes, str):
-            notes = [notes]
-
-        notes_to_play = []
-
-        for note in notes:
-            instrument_instance = self.notes[note]['instrument']
-            note_instrument = self.notes[note]['note']
-
-            if not instrument_instance:
-                continue
-
-            note_to_play = instrument_instance.note_to_play(note_instrument)
-            notes_to_play.append(note_to_play)
+KEY_FREQUENCY_MAP = {
+    9: librosa.note_to_hz('B3'),
+    113: librosa.note_to_hz('C4'),
+    50: librosa.note_to_hz('C#4'),
+    119: librosa.note_to_hz('D4'),
+    51: librosa.note_to_hz('D#4'),
+    101: librosa.note_to_hz('E4'),
+    114: librosa.note_to_hz('F4'),
+    53: librosa.note_to_hz('F#4'),
+    116: librosa.note_to_hz('G4'),
+    54: librosa.note_to_hz('G#4'),
+    121: librosa.note_to_hz('A4'),
+    55: librosa.note_to_hz('A#4'),
+    117: librosa.note_to_hz('B4'),
+    105: librosa.note_to_hz('C5'),
+    57: librosa.note_to_hz('C#5'),
+    111: librosa.note_to_hz('D5'),
+    48: librosa.note_to_hz('D#5'),
+    112: librosa.note_to_hz('E5'),
+    122: librosa.note_to_hz('F5'),
+    115: librosa.note_to_hz('F#5'),
+    120: librosa.note_to_hz('G5'),
+    100: librosa.note_to_hz('G#5'),
+    99: librosa.note_to_hz('A5'),
+    102: librosa.note_to_hz('A#5'),
+    118: librosa.note_to_hz('B5'),
+    98: librosa.note_to_hz('C6'),
+    104: librosa.note_to_hz('C#6'),
+    110: librosa.note_to_hz('D6'),
+    106: librosa.note_to_hz('D#6'),
+    109: librosa.note_to_hz('E6'),
+    44: librosa.note_to_hz('F6'),
+    108: librosa.note_to_hz('F#6'),
+    46: librosa.note_to_hz('G6'),
+    303: librosa.note_to_hz('G#6'),
+    59: librosa.note_to_hz('A6'),
+    304: librosa.note_to_hz('B6'),
+}
 
 
-class Instrument:
-    def __init__(self, name, note_range, octave_changer=True):
-        self.name = name
-        self.note_range = note_range
-        self.notes = {note: None for note in list_notes(note_range)}
-        self.octave_changer = octave_changer
-
-    def note_to_play(self, note):
-        return [self, note]
+def get_sin_oscillator(freq=55, amp=1, sample_rate=SAMPLE_RATE):
+    increment = (2 * math.pi * freq) / sample_rate
+    return (
+        math.sin(v) * amp * NOTE_AMP for v in itertools.count(start=0, step=increment)
+    )
 
 
-class Piano(Instrument):
-    def __init__(self):
-        super().__init__(name='Piano', note_range='C0-B9', octave_changer=True)
+def get_samples(notes_dict, num_samples=BUFFER_SIZE):
+    return [
+        sum([int(next(osc) * 32767) for _, osc in notes_dict.items()])
+        for _ in range(num_samples)
+    ]
+
+class ThreadFunc:
+    def __init__(self, notes_dict):
+        self.run = True
+        self.t1 = None
+        self.notes_dict = notes_dict
+        self.audio = pyaudio.PyAudio()
+        self.stream = self.audio.open(
+                rate=SAMPLE_RATE,
+                channels=1,
+                format=pyaudio.paInt16,
+                output=True,
+                frames_per_buffer=BUFFER_SIZE,
+        )
+
+    def func(self):
+        while self.run:
+            if self.notes_dict:
+                samples = get_samples(self.notes_dict)
+                samples = np.int16(samples).tobytes()
+                self.stream.write(samples)
+
+    def go(self):
+        self.t1 = threading.Thread(target=self.func)
+        self.t1.start()
+
+    def stop(self):
+        self.run = False
+        self.t1.stop()
+        self.stream.close()
+        self.audio.terminate()
 
 
-class Drums(Instrument):
-    def __init__(self):
-        super().__init__(name='Drums', note_range='B0-A4', octave_changer=False)
+class SynthWidget(Widget):
+    def __init__(self, **kwargs):
+        super(SynthWidget, self).__init__(**kwargs)
+        self.notes_dict = {}
+        self.last_notes = {}
+        Window.bind(on_key_down=self.key_down_action)
+        Window.bind(on_key_up=self.key_up_action)
+        self.tf = ThreadFunc(self.notes_dict)
+        self.tf.go()
+
+    def key_down_action(self, window, key, scancode, codepoint, modifier):
+        if key in KEY_FREQUENCY_MAP:
+            freq = KEY_FREQUENCY_MAP[key]
+            if key not in self.notes_dict:
+                self.notes_dict[key] = get_sin_oscillator(freq=freq, amp=1)
+                print(len(self.notes_dict), self.notes_dict)
+
+    def key_up_action(self, window, key, scancode):
+        if key in self.notes_dict:
+            del self.notes_dict[key]
+            print(len(self.notes_dict), self.notes_dict)
+
+    def on_stop(self):
+        self.tf.stop()
+
+
+class MyApp(App):
+    def build(self):
+        return SynthWidget()
 
 
 if __name__ == '__main__':
-    computer_piano = ComputerPiano()
-    computer_piano.add_instrument(Piano())
-    computer_piano.add_instrument(Drums(), 'C0-A#3')
-    computer_piano.play(['C0', 'B2', 'G3', 'A#4', 'D#5'])
-    
+    try:
+        MyApp().run()
+    except KeyboardInterrupt:
+        print("Stopping...")
